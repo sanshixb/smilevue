4b7bdd9fd6a11fade9aab70a58daa4767dac5298
unknown <wb-wl328831@6CR525614M.hz.ali.com>
2019-06-28 18:20:55 +0800

    aa

---
+++ src/serviceAPI.config.js
@@ -0,0 +1,7 @@
+const BASEURL = "https://www.easy-mock.com/mock/5d09f4cab7d8a17c2b879056/SmileVue/"
+const URL = {
+    getShoppingMallInfo:BASEURL+'index',
+    getGoodsInfo:BASEURL+'getGoodsInfo'
+}
+
+module.exports = URL
